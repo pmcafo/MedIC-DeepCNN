878d00789654637423a9a68dcbc93c2e260fb5f0
pmcafo <pmcafo@proton.me>
2020-03-08 12:48:52 +0000

    shellcheck

--- ensmic/data_loading/architectures.py
+++ ensmic/data_loading/architectures.py
@@ -1,2 +1,8 @@
 #==============================================================================#
-#  A
+#  Author:       Dominik Müller                                                #
+#  Copyright:    2021 IT-Infrastructure for Translational Medical Research,    #
+#                University of Augsburg                                        #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either vers
