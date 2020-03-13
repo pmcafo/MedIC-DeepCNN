7ad2b91e7212ae14d9408ad6110beda7671ae241
pmcafo <pmcafo@proton.me>
2020-03-13 10:21:40 +0000

    Prepare 2.2.1 release

--- ensmic/data_loading/io_inference.py
+++ ensmic/data_loading/io_inference.py
@@ -13,4 +13,16 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
 #  GNU General Public License for more details.                                #
 #                                                                              #
-#  You should have received a copy of t
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#==============================================================================#
+#-----------------------------------------------------#
+#                   Library imports                   #
+#-----------------------------------------------------#
+# External libraries
+import os
+import json
+import numpy as np
+
+#-----------------------------------------------------#
+#               Inference I
