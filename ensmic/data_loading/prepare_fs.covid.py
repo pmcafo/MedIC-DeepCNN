c899efe8bd7ec99342b4f5eb819a00853fed1126
pmcafo <pmcafo@proton.me>
2020-03-26 17:17:16 +0000

    modified CHANGES.txt

--- ensmic/data_loading/prepare_fs.covid.py
+++ ensmic/data_loading/prepare_fs.covid.py
@@ -1,2 +1,9 @@
 #==============================================================================#
-#  Author:       Dominik Müller                                                
+#  Author:       Dominik Müller                                                #
+#  Copyright:    2021 IT-Infrastructure for Translational Medical Research,    #
+#                University of Augsburg                                        #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later 
