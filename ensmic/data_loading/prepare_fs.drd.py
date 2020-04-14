b1808863ed8c2925efb7c9814da193d38fb7e90b
pmcafo <pmcafo@proton.me>
2020-04-14 14:59:54 +0000

    gitpush

--- ensmic/data_loading/prepare_fs.drd.py
+++ ensmic/data_loading/prepare_fs.drd.py
@@ -1,3 +1,10 @@
 #==============================================================================#
 #  Author:       Dominik Müller                                                #
-#  Copyright:    2021 IT-Infrastructure for Translational Medic
+#  Copyright:    2021 IT-Infrastructure for Translational Medical Research,    #
+#                University of Augsburg                                        #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#           
