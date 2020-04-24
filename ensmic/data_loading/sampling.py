766195bd74ed1c632f95ee7ee3f76bee717b5ce1
pmcafo <pmcafo@proton.me>
2020-04-24 08:40:13 +0000

    removed lib

--- ensmic/data_loading/sampling.py
+++ ensmic/data_loading/sampling.py
@@ -3,4 +3,10 @@
 #  Copyright:    2021 IT-Infrastructure for Translational Medical Research,    #
 #                University of Augsburg                                        #
 #                                                                              #
-#  This program is f
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; witho
