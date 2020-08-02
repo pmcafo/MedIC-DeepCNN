d86331c0e1fd7fd745ad199b73da8059f7e3dd88
pmcafo <pmcafo@proton.me>
2020-08-02 07:34:43 +0000

    smpboot, idle: Optimize calls to smp_processor_id() in idle_threads_init()

--- ensmic/ensemble/k_neighbors.py
+++ ensmic/ensemble/k_neighbors.py
@@ -1,1 +1,7 @@
-#==========================
+#==============================================================================#
+#  Author:       Dominik Müller                                                #
+#  Copyright:    2020 IT-Infrastructure for Translational Medical Research,    #
+#                University of Augsburg                                        #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by  
