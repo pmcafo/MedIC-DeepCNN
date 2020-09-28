d10f65d514dbe68e5c968505ac0c71362313551d
pmcafo <pmcafo@proton.me>
2020-09-28 05:35:13 +0000

    0.3.22

--- ensmic/ensemble/majorityvote_soft.py
+++ ensmic/ensemble/majorityvote_soft.py
@@ -14,4 +14,15 @@
 #  GNU General Public License for more details.                                #
 #                                                                              #
 #  You should have received a copy of the GNU General Public License           #
-#  along 
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#==============================================================================#
+#-----------------------------------------------------#
+#                   Library imports                   #
+#-----------------------------------------------------#
+# External libraries
+import pandas as pd
+# Internal libraries/scripts
+from ensmic.ensemble.abstract_elm import Abstract_Ensemble
+from ensmic.utils.softmax import softmax
+
+#------------------------------------------
