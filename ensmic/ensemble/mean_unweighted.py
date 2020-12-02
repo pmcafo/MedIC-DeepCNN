852810ac836ade372f71bbb93640f5e38967473b
pmcafo <pmcafo@proton.me>
2020-12-02 21:12:36 +0000

    f2fs: refactor f2fs_commit_super

--- ensmic/ensemble/mean_unweighted.py
+++ ensmic/ensemble/mean_unweighted.py
@@ -48,4 +48,14 @@ class ELM_MeanUnweighted(Abstract_Ensemble):
     #                  Training                   #
     #---------------------------------------------#
     def training(self, train_x, train_y):
-        # 
+        # No training required for this method, therefore skip
+        pass
+
+    #---------------------------------------------#
+    #                  Prediction                 #
+    #---------------------------------------------#
+    def prediction(self, data):
+        # Split data columns into multi level structure based on architecutre
+        data.columns = data.columns.str.split('_', expand=True)
+        # Compute average class probability (mean) across all architectures
+        pred = data.groupby
