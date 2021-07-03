591b18156526b527e590ccfa5a679663ebaba4af
pmcafo <pmcafo@proton.me>
2021-07-03 12:21:24 +0000

    fix unit tests

--- ensmic/ensemble/support_vector_machine.py
+++ ensmic/ensemble/support_vector_machine.py
@@ -2,4 +2,10 @@
 #  Author:       Dominik Müller                                                #
 #  Copyright:    2020 IT-Infrastructure for Translational Medical Research,    #
 #                University of Augsburg                                        #
-#               
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful
