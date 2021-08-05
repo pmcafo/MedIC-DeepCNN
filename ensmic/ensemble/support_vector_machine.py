a29ffc9ec418a1f12819b207604cef924ca6a7b0
pmcafo <pmcafo@proton.me>
2021-08-05 19:28:14 +0000

    Release 0.3.1

--- ensmic/ensemble/support_vector_machine.py
+++ ensmic/ensemble/support_vector_machine.py
@@ -27,4 +27,13 @@ from sklearn.svm import SVC
 from ensmic.ensemble.abstract_elm import Abstract_Ensemble
 
 #-----------------------------------------------------#
-#         
+#             ELM: Support Vector Machine             #
+#-----------------------------------------------------#
+""" Ensemble Learning approach via Support Vector Machine.
+
+Methods:
+    __init__                Initialize Ensemble Learning Method.
+    training:               Fit Ensemble Learning Method on validate-ensemble.
+    prediction:             Utilize Ensemble Learning Method for test dataset.
+    dump:                   Save (fitted) model to disk.
+    load:                   Load (fitted) model
