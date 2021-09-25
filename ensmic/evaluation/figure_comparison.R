0098eddb390de8a9fc909939c816563fd61eb45a
pmcafo <pmcafo@proton.me>
2021-09-25 09:56:06 +0000

    sched_clock: Prevent 64bit inatomicity on 32bit systems

--- ensmic/evaluation/figure_comparison.R
+++ ensmic/evaluation/figure_comparison.R
@@ -106,4 +106,22 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
   numPlots = length(plots)
   
   # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout))
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix
