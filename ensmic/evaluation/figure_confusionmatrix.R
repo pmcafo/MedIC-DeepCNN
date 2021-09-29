ac5fe9ccec631718205f78819d66db6bedf33d56
pmcafo <pmcafo@proton.me>
2021-09-29 07:01:21 +0000

    Fixed a spelling mistake.

--- ensmic/evaluation/figure_confusionmatrix.R
+++ ensmic/evaluation/figure_confusionmatrix.R
@@ -7,4 +7,18 @@ library("gridExtra")
 library("stringr")
 
 # Configurations
-path_results <- "/home/mudomini/projects/ensmic/results/
+path_results <- "/home/mudomini/projects/ensmic/results/"
+datasets <- c("chmnist", "covid", "isic", "drd")
+phases <- c("baseline", "augmenting", "stacking", "bagging")
+
+path_eval <- file.path(path_results, "evaluation")
+dir.create(path_eval, showWarnings=FALSE)
+
+# Load data
+dt <- fread(file.path(path_results, "eval_tmp", "confusion_matrix.csv"))
+
+
+# Preprocess
+dt$dataset <- paste("Dataset:", dt$dataset)
+dt <- transform(dt, dataset=factor(dataset, levels=c("Dataset: CHMNIST","Dataset: COVID",
+                                
