c1ab74e337ed85c99ff7576e32da246ba4f67687
pmcafo <pmcafo@proton.me>
2021-10-08 19:34:48 +0000

    Next

--- ensmic/evaluation/identify_epochs.py
+++ ensmic/evaluation/identify_epochs.py
@@ -1,1 +1,8 @@
-#==========================================================
+#==============================================================================#
+#  Author:       Dominik Müller                                                #
+#  Copyright:    2021 IT-Infrastructure for Translational Medical Research,    #
+#                University of Augsburg                                        #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  t
