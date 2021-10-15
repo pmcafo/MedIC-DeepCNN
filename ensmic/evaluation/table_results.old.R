ce63471da90a420ba03cb12c46daf4cb72ee5320
pmcafo <pmcafo@proton.me>
2021-10-15 20:43:48 +0000

    0.10.7

--- ensmic/evaluation/table_results.old.R
+++ ensmic/evaluation/table_results.old.R
@@ -1,2 +1,20 @@
 # Import libraries
-library("
+library("data.table")
+library("ggplot2")
+library("tidyr")
+library("dplyr")
+
+# Configurations
+path_results <- "/home/mudomini/projects/ensmic/results/"
+datasets <- c("chmnist", "covid", "isic", "drd")
+phases <- c("baseline", "augmenting", "stacking", "bagging")
+
+path_eval <- file.path(path_results, "evaluation")
+dir.create(path_eval, showWarnings=FALSE)
+
+# Gather results for baseline, augmenting & stacing
+for (i in seq(1,3)){
+  dt <- data.table()
+  
+  for (j in seq(1,4)){
+    path_dir <-
