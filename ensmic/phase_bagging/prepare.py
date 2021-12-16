05017eb271727b4fa8370724762a158e758e5c82
pmcafo <pmcafo@proton.me>
2021-12-16 14:54:36 +0000

    doc typo fix

--- ensmic/phase_bagging/prepare.py
+++ ensmic/phase_bagging/prepare.py
@@ -4,4 +4,10 @@
 #                University of Augsburg                                        #
 #                                                                              #
 #  This program is free software: you can redistribute it and/or modify        #
-#  it under the terms of the GNU General Public License as publis
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             
