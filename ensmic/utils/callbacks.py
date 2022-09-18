192682d791934812af274ff2ba9224db4dfd16e3
pmcafo <pmcafo@proton.me>
2022-09-18 18:44:37 +0000

    Added views

--- ensmic/utils/callbacks.py
+++ ensmic/utils/callbacks.py
@@ -13,4 +13,14 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
 #  GNU General Public License for more details.                                #
 #                                                                              #
-#  You should have received a copy of the GNU General Public License
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#==============================================================================#
+#-----------------------------------------------------#
+#                   Library imports                   #
+#-----------------------------------------------------#
+from tensorflow.keras.callbacks import EarlyStopping
+
+#-----------------------------------------------------#
+#                   Custom Callbacks                  #
+#-------------------
